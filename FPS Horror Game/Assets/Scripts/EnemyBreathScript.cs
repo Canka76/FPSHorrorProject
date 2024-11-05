587e01c4f839e231a6d52482ad17a737610bf6ec
Canka <karakoccan04@gmail.com>
2024-11-05 23:47:27 +0300

    breathing sounds and script done

--- FPS Horror Game/Assets/Scripts/EnemyBreathScript.cs
+++ FPS Horror Game/Assets/Scripts/EnemyBreathScript.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class EnemyBreathScript : BreatheAction
 {
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip[] breathClips;
+    [SerializeField] private float breathCooldown = 2.0f; // Cooldown duration in seconds
+    
+    private int randomClip;
+    private float lastBreathTime;
+
     public override void OnFocusEnemy()
     {
-       print("looking at enemy" + gameObject.name);
+        // Check if cooldown has elapsed
+        if (Time.time - lastBreathTime >= breathCooldown)
+        {
+            // Play a random breathing sound if there are clips
+            if (breathClips.Length > 0)
+            {
+                randomClip = Random.Range(0, breathClips.Length);
+                _audioSource.PlayOneShot(breathClips[randomClip]);
+                lastBreathTime = Time.time; // Reset cooldown timer
+            }
+            else
+            {
+                Debug.LogWarning("No breath clips assigned in EnemyBreathScript.");
+            }
+        }
     }
 
     public override void OnLoseFocusEnemy()
     {
-        print("Stopped looking at enemy" + gameObject.name);
-
+        Debug.Log("Stopped looking at enemy: " + gameObject.name);
     }
 }
